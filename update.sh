045a8503face5edbac82116b0ff163fa975b0c6f
Arjan van de Ven <arjan@linux.intel.com>
2018-12-10 13:29:58 +0000

    git pull first

--- update.sh
+++ update.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-
+git pull
 VERSION=`curl -s http://ftp.mozilla.org/pub/thunderbird/releases/ | grep href | cut -f3 -d">" | cut -f1 -d"/" | grep -v "b" | sort -n | tail -1`
 
 if [[ -z "${VERSION}" ]]; then
