2a4363830e34626efd5a1e3dfb7d46e91282d755
Patrick McCarty <patrick.mccarty@intel.com>
2020-07-01 11:31:18 -0700

    Make update.sh for resilient to failure; fix version sort
    The latest release of Thunderbird is 68.10.0, and running `sort -n`
    command does not result 68.10.0 being the last line of sorted output...
    
    Instead filter the index page for valid, non-beta releases, set the
    field separator to '.', and numeric sort each field in order. Then,
    68.10.0 appears on the last line. This approach should work moving
    forward.
    
    Signed-off-by: Patrick McCarty <patrick.mccarty@intel.com>

--- update.sh
+++ update.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
+
+set -e
+set -o pipefail
+
 git pull
-VERSION=`curl -s http://ftp.mozilla.org/pub/thunderbird/releases/ | grep href | cut -f3 -d">" | cut -f1 -d"/" | grep -v "b" | sort -n | tail -1`
+VERSION=$(curl -sSf http://ftp.mozilla.org/pub/thunderbird/releases/ | grep href | cut -f3 -d">" | cut -f1 -d"/" | grep -Ex '[0-9.]+' | sort -t . -k 1,1n -k 2,2n -k 3,3n -k 4,4n | tail -1)
 
 if [[ -z "${VERSION}" ]]; then
     echo "Please pass the new version first."
@@ -10,7 +14,7 @@ fi
 sed thunderbird.spec.in -e "s/\#\#VERSION\#\#/${VERSION}/g" > thunderbird.spec
 make generateupstream || exit 1
 
+make bumpnogit
 git add thunderbird.spec Makefile release upstream
 git commit -s -m "Update to ${VERSION}"
-#make bump
 make koji
